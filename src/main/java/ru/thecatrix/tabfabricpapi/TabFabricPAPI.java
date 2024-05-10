35f4d4eb546b5618540a4ddd2f01ba30e47ac2c2
CatRiX <58901923+TheCatRiX@users.noreply.github.com>
2024-05-10 20:15:10 +0300

    Reformat code

--- src/main/java/ru/thecatrix/tabfabricpapi/TabFabricPAPI.java
+++ src/main/java/ru/thecatrix/tabfabricpapi/TabFabricPAPI.java
@@ -13,33 +13,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TabFabricPAPI implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("tabfabricpapi");
 
-	@Override
-	public void onInitialize() {
-		LOGGER.info("TabFabricPAPI initialized");
+    @Override
+    public void onInitialize() {
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+            registerPlaceholders(server);
 
-		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
-			registerPlaceholders(server);
+            TabAPI.getInstance().getEventBus().register(TabLoadEvent.class, event -> {
+                LOGGER.info("[TabFabricPAPI] TabLoadEvent triggered");
+                registerPlaceholders(server);
+            });
+        });
 
-			TabAPI.getInstance().getEventBus().register(TabLoadEvent.class, event -> {
-				LOGGER.info("[TabFabricPAPI] TabLoadEvent triggered");
-				registerPlaceholders(server);
-			});
-		});
-	}
+        LOGGER.info("TabFabricPAPI initialized");
+    }
 
-	private void registerPlaceholders(MinecraftServer server) {
-		TabAPI.getInstance().getPlaceholderManager().registerServerPlaceholder("%vanish_online%", 1000, () -> Placeholders.parseText(Text.of("%vanish:online%"), PlaceholderContext.of(server)).getString());
-		LOGGER.info("[TabFabricPAPI] Placeholder %vanish_online% registered");
+    private void registerPlaceholders(MinecraftServer server) {
+        TabAPI.getInstance().getPlaceholderManager().registerServerPlaceholder("%vanish_online%", 1000, () -> Placeholders.parseText(Text.of("%vanish:online%"), PlaceholderContext.of(server)).getString());
+        LOGGER.info("[TabFabricPAPI] Placeholder %vanish_online% registered");
 
-		TabAPI.getInstance().getPlaceholderManager().registerServerPlaceholder("%spark_tps%", 1000, () -> Placeholders.parseText(Text.of("%spark:tps 10s%"), PlaceholderContext.of(server)).getString().replace("*", ""));
-		LOGGER.info("[TabFabricPAPI] Placeholder %spark_tps% registered");
+        TabAPI.getInstance().getPlaceholderManager().registerServerPlaceholder("%spark_tps%", 1000, () -> Placeholders.parseText(Text.of("%spark:tps 10s%"), PlaceholderContext.of(server)).getString().replace("*", ""));
+        LOGGER.info("[TabFabricPAPI] Placeholder %spark_tps% registered");
 
-		TabAPI.getInstance().getPlaceholderManager().registerServerPlaceholder("%spark_cpu%", 1000, () -> Placeholders.parseText(Text.of("%spark:cpu_process 10s%"), PlaceholderContext.of(server)).getString().replace("%", ""));
-		LOGGER.info("[TabFabricPAPI] Placeholder %spark_cpu% registered");
-	}
+        TabAPI.getInstance().getPlaceholderManager().registerServerPlaceholder("%spark_cpu%", 1000, () -> Placeholders.parseText(Text.of("%spark:cpu_process 10s%"), PlaceholderContext.of(server)).getString().replace("%", ""));
+        LOGGER.info("[TabFabricPAPI] Placeholder %spark_cpu% registered");
+    }
 }
